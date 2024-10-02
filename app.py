cbc11a50b6f5ac08df9d5c0a051a9e3dcf3d3550
Moneda-mecanica <90118483+Moneda-mecanica@users.noreply.github.com>
2024-10-02 16:41:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ url = 'http://127.0.0.1:8050'
 #    webbrowser.open(url)
     
 app = dash.Dash(  __name__, meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}], prevent_initial_callbacks='initial_duplicate',  suppress_callback_exceptions=True)
-
+server = app.server
 Aplicativo(app, url, ejecucion_demanda_parte_2, dem_total)
 
 if __name__ == "__main__":
